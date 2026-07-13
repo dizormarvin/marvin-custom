2464892959c7adc03d122b590dbbb18ce3ff25a9
Marvin <dizormarvin@gmail.com>
2026-07-13 12:43:45 +0800

    Update first_doctype.js

--- marvin_custom/first_custom/doctype/first_doctype/first_doctype.js
+++ marvin_custom/first_custom/doctype/first_doctype/first_doctype.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2026, Marvin and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("First Doctype", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("First Doctype", {
+	refresh(frm) {
+        console.log('test')
+	},
+});
